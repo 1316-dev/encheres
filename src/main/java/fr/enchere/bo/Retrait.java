4f11faf0a493af71d93773d35eec52c6c8ee023b
Helena <helena.lecable@gmail.com>
2025-12-15 13:54:05 +0100

    Mise à jour Bo

--- src/main/java/fr/enchere/bo/Retrait.java
+++ src/main/java/fr/enchere/bo/Retrait.java
@@ -6,21 +6,21 @@ public class Retrait {
     private String rue;
     private String cp;
     private String ville;
+    private int noArticle;
 
-    public Retrait(){
-
+    public Retrait() {
     }
-    public Retrait(String rue, String cp, String ville) {
+
+    public Retrait(String rue, String cp, String ville, int noArticle) {
         this.rue = rue;
         this.cp = cp;
         this.ville = ville;
+        this.noArticle = noArticle;
     }
 
-    //Getters and setters
     public String getRue() {
         return rue;
     }
-
     public void setRue(String rue) {
         this.rue = rue;
     }
@@ -28,7 +28,6 @@ public class Retrait {
     public String getCp() {
         return cp;
     }
-
     public void setCp(String cp) {
         this.cp = cp;
     }
@@ -36,12 +35,29 @@ public class Retrait {
     public String getVille() {
         return ville;
     }
-
     public void setVille(String ville) {
         this.ville = ville;
     }
 
-    //To string et equals
+    public int getNoArticle() {
+        return noArticle;
+    }
+    public void setNoArticle(int noArticle) {
+        this.noArticle = noArticle;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Retrait retrait = (Retrait) o;
+        return noArticle == retrait.noArticle && Objects.equals(rue, retrait.rue) && Objects.equals(cp, retrait.cp) && Objects.equals(ville, retrait.ville);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rue, cp, ville, noArticle);
+    }
+
 
     @Override
     public String toString() {
@@ -49,18 +65,7 @@ public class Retrait {
                 "rue='" + rue + '\'' +
                 ", cp='" + cp + '\'' +
                 ", ville='" + ville + '\'' +
+                ", noArticle=" + noArticle +
                 '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Retrait retrait = (Retrait) o;
-        return Objects.equals(rue, retrait.rue) && Objects.equals(cp, retrait.cp) && Objects.equals(ville, retrait.ville);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(rue, cp, ville);
     }
 }
