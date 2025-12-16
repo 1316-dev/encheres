47e97f2fe46ec6bf5b6c7f1c357243f2bfcdfa27
1316-dev <mathieu.pineau@hotmail.fr>
2025-12-16 10:29:43 +0100

    ajout attribut categorieArticle dans article Dto

--- src/main/java/fr/enchere/dto/ArticleDto.java
+++ src/main/java/fr/enchere/dto/ArticleDto.java
@@ -1,5 +1,6 @@
 package fr.enchere.dto;
 
+import fr.enchere.bo.Categorie;
 import fr.enchere.bo.Enchere;
 import fr.enchere.bo.Retrait;
 import fr.enchere.bo.Utilisateur;
@@ -16,23 +17,26 @@ public class ArticleDto {
         private int miseAPrix;
 
         private Utilisateur utilisateur;
+        private Categorie categorieArticle;
 
     public ArticleDto() {
     }
 
-    public ArticleDto(String nomArticle, LocalDateTime dateFinEnchere, int miseAPrix, Utilisateur utilisateur) {
+    public ArticleDto(String nomArticle, LocalDateTime dateFinEnchere, int miseAPrix, Utilisateur utilisateur, Categorie categorieArticle) {
         this.nomArticle = nomArticle;
         this.dateFinEnchere = dateFinEnchere;
         this.miseAPrix = miseAPrix;
         this.utilisateur = utilisateur;
+        this.categorieArticle = categorieArticle;
     }
 
-    public ArticleDto(int noArticle, String nomArticle, LocalDateTime dateFinEnchere, int miseAPrix, Utilisateur utilisateur) {
+    public ArticleDto(int noArticle, String nomArticle, LocalDateTime dateFinEnchere, int miseAPrix, Utilisateur utilisateur, Categorie categorieArticle) {
         this.noArticle = noArticle;
         this.nomArticle = nomArticle;
         this.dateFinEnchere = dateFinEnchere;
         this.miseAPrix = miseAPrix;
         this.utilisateur = utilisateur;
+        this.categorieArticle = categorieArticle;
     }
 
     public int getNoArticle() {
@@ -75,16 +79,12 @@ public class ArticleDto {
         this.utilisateur = utilisateur;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        ArticleDto that = (ArticleDto) o;
-        return noArticle == that.noArticle && miseAPrix == that.miseAPrix && Objects.equals(nomArticle, that.nomArticle) && Objects.equals(dateFinEnchere, that.dateFinEnchere) && Objects.equals(utilisateur, that.utilisateur);
+    public Categorie getCategorieArticle() {
+        return categorieArticle;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(noArticle, nomArticle, dateFinEnchere, miseAPrix, utilisateur);
+    public void setCategorieArticle(Categorie categorieArticle) {
+        this.categorieArticle = categorieArticle;
     }
 
     @Override
@@ -95,6 +95,19 @@ public class ArticleDto {
                 ", dateFinEnchere=" + dateFinEnchere +
                 ", miseAPrix=" + miseAPrix +
                 ", utilisateur=" + utilisateur +
+                ", categorieArticle=" + categorieArticle +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ArticleDto that = (ArticleDto) o;
+        return noArticle == that.noArticle && miseAPrix == that.miseAPrix && Objects.equals(nomArticle, that.nomArticle) && Objects.equals(dateFinEnchere, that.dateFinEnchere) && Objects.equals(utilisateur, that.utilisateur) && Objects.equals(categorieArticle, that.categorieArticle);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(noArticle, nomArticle, dateFinEnchere, miseAPrix, utilisateur, categorieArticle);
     }
 }
