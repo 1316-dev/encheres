fe5cffd0b9885a5a8e1765b3e48ee69809f11079
1316-dev <mathieu.pineau@hotmail.fr>
2025-12-16 14:30:29 +0100

    EnchereController.java devient AccueilController

--- src/main/java/fr/enchere/controller/AccueilController.java
+++ src/main/java/fr/enchere/controller/AccueilController.java
@@ -7,15 +7,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class EnchereController {
+public class AccueilController {
 
     private final ArticleVenduService articleVenduService;
 
-    public EnchereController(ArticleVenduService articleVenduService) {
+    public AccueilController(ArticleVenduService articleVenduService) {
         this.articleVenduService = articleVenduService;
     }
 
