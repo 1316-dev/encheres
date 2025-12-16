f65896532f0bcfb6f6a91bf8e2b4da94f216ef48
Helena <helena.lecable@gmail.com>
2025-12-16 15:34:46 +0100

    methode post sur formulaire inscription

--- src/main/java/fr/enchere/security/WebSecurityConfig.java
+++ src/main/java/fr/enchere/security/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/encheres","/inscription ","/css/*", "/images/*").permitAll()
+                        .requestMatchers("/", "/encheres","/inscription","/css/*", "/images/*").permitAll()
                         .requestMatchers(HttpMethod.GET, "/","/encheres").permitAll()
                        // .requestMatchers( "/profil","/vendre","/").hasRole("ADMIN")
                         .anyRequest().authenticated()
