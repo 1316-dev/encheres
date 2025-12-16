7f5ca30d51b6ef1eb07a37190ce9a0b83bb0f653
Helena <helena.lecable@gmail.com>
2025-12-16 09:12:12 +0100

    Modif create-table, création DB, maj html

--- sql/create-table.sql
+++ sql/create-table.sql
@@ -1,17 +1,19 @@
 -- Script de création de la base de données ENCHERES
 --   type :      SQL Server 2012
---
 
+/* Suppression des tables si necessaire */
+DROP TABLE RETRAITS;
+DROP TABLE ENCHERES;
+DROP TABLE ARTICLES_VENDUS;
+DROP TABLE CATEGORIES;
+DROP TABLE UTILISATEURS;
 
-
+/* Création des tables */
 CREATE TABLE CATEGORIES (
                             no_categorie   INTEGER IDENTITY(1,1) NOT NULL,
                             libelle        VARCHAR(30) NOT NULL
 )
-
 ALTER TABLE CATEGORIES ADD constraint categorie_pk PRIMARY KEY (no_categorie)
-
-
 
 CREATE TABLE UTILISATEURS (
                               no_utilisateur   INTEGER IDENTITY(1,1) NOT NULL,
@@ -23,30 +25,25 @@ CREATE TABLE UTILISATEURS (
                               rue              VARCHAR(30) NOT NULL,
                               code_postal      VARCHAR(10) NOT NULL,
                               ville            VARCHAR(50) NOT NULL,
-                              mot_de_passe     VARCHAR(30) NOT NULL,
-                              credit           INTEGER NOT NULL,
+                              mot_de_passe     VARCHAR(100) NOT NULL,
+                              credit           INTEGER NOT NULL default 100,
                               administrateur   bit NOT NULL
 )
-
 ALTER TABLE UTILISATEURS ADD constraint utilisateur_pk PRIMARY KEY (no_utilisateur)
-
-
+ALTER TABLE UTILISATEURS ADD constraint utilisateur_uq UNIQUE(email,pseudo)
 CREATE TABLE ARTICLES_VENDUS (
                                  no_article                    INTEGER IDENTITY(1,1) NOT NULL,
                                  nom_article                   VARCHAR(30) NOT NULL,
                                  description                   VARCHAR(300) NOT NULL,
                                  date_debut_encheres           DATE NOT NULL,
                                  date_fin_encheres             DATE NOT NULL,
-                                 prix_initial                  INTEGER,
+                                 prix_initial                  INTEGER NOT NULL,
                                  prix_vente                    INTEGER,
                                  no_utilisateur                INTEGER NOT NULL,
                                  no_categorie                  INTEGER NOT NULL
 )
 
-
-
 ALTER TABLE ARTICLES_VENDUS ADD constraint articles_vendus_pk PRIMARY KEY (no_article)
-
 
 CREATE TABLE RETRAITS (
                           no_article       INTEGER NOT NULL,
@@ -54,13 +51,11 @@ CREATE TABLE RETRAITS (
                           code_postal      VARCHAR(15) NOT NULL,
                           ville            VARCHAR(30) NOT NULL
 )
-
 ALTER TABLE RETRAITS ADD constraint retrait_pk PRIMARY KEY  (no_article)
 ALTER TABLE RETRAITS
     ADD CONSTRAINT retrait_article_fk FOREIGN KEY ( no_article ) REFERENCES  ARTICLES_VENDUS (no_article)
         ON DELETE NO ACTION
         ON UPDATE no action
-
 
 CREATE TABLE ENCHERES(
                          no_enchere  INTEGER IDENTITY(1,1) NOT NULL,
@@ -69,30 +64,23 @@ CREATE TABLE ENCHERES(
                          no_article INTEGER NOT NULL,
                          no_utilisateur INTEGER NOT NULL
 )
-
 ALTER TABLE ENCHERES ADD constraint enchere_pk PRIMARY KEY ( no_enchere)
-
 ALTER TABLE ENCHERES
     ADD CONSTRAINT encheres_utilisateur_fk FOREIGN KEY ( no_utilisateur ) REFERENCES UTILISATEURS ( no_utilisateur )
         ON DELETE NO ACTION
         ON UPDATE no action
-
 ALTER TABLE ENCHERES
     ADD CONSTRAINT encheres_no_article_fk FOREIGN KEY ( no_article ) REFERENCES ARTICLES_VENDUS ( no_article )
         ON DELETE NO ACTION
         ON UPDATE no action
-
 
 ALTER TABLE ARTICLES_VENDUS
     ADD CONSTRAINT articles_vendus_categories_fk FOREIGN KEY ( no_categorie )
         REFERENCES categories ( no_categorie )
         ON DELETE NO ACTION
         ON UPDATE no action
-
 ALTER TABLE ARTICLES_VENDUS
     ADD CONSTRAINT ventes_utilisateur_fk FOREIGN KEY ( no_utilisateur )
         REFERENCES utilisateurs ( no_utilisateur )
         ON DELETE NO ACTION
         ON UPDATE no action
-
-
