f9ab0bc7c36ada186cd8abcb7c552418c1e07885
1316-dev <mathieu.pineau@hotmail.fr>
2025-12-16 11:47:12 +0100

    mise à jout view sql en ajoutant no_article

--- src/main/resources/sql/view-afficherVentesEnCours.sql
+++ src/main/resources/sql/view-afficherVentesEnCours.sql
@@ -1,6 +1,6 @@
 CREATE VIEW afficherVentesEnCours
 AS
-select nom_article, prix_initial, date_fin_encheres, utilisateurs.pseudo AS Vendeur
+select no_article,nom_article, prix_initial, date_fin_encheres, utilisateurs.pseudo AS Vendeur
 from ARTICLES_VENDUS
 inner join UTILISATEURS on UTILISATEURS.no_utilisateur = UTILISATEURS.no_utilisateur
 GO
