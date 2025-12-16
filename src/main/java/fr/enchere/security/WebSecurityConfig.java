39b9d3881baa9c412e60c28fb2588f3d6275ed98
1316-dev <mathieu.pineau@hotmail.fr>
2025-12-16 14:33:59 +0100

    WebSecurityConfig ajout de la methode mapper encheresCategories

--- src/main/java/fr/enchere/security/WebSecurityConfig.java
+++ src/main/java/fr/enchere/security/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/encheres","/inscription ","/css/*", "/images/*").permitAll()
+                        .requestMatchers("/", "/encheres","/inscription","/css/*", "/images/*","/encheresCategorie").permitAll()
                         .requestMatchers(HttpMethod.GET, "/","/encheres").permitAll()
                        // .requestMatchers( "/profil","/vendre","/").hasRole("ADMIN")
                         .anyRequest().authenticated()
