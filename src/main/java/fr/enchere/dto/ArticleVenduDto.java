1b45e8dc5850177ca1fa19dbaff93c07741723c1
1316-dev <mathieu.pineau@hotmail.fr>
2025-12-17 13:50:03 +0100

    maj attribut ArticleVenduDto

--- src/main/java/fr/enchere/dto/ArticleVenduDto.java
+++ src/main/java/fr/enchere/dto/ArticleVenduDto.java
@@ -1,6 +1,8 @@
 package fr.enchere.dto;
 
 
+import fr.enchere.bo.Retrait;
+
 import java.util.Date;
 import java.util.Objects;
 
@@ -10,21 +12,27 @@ public class ArticleVenduDto {
         private String nomArticle;
         private Date dateFinEnchere;
         private int miseAPrix;
-
         private String vendeur;
         private int noCategorie;
-
+        private String description;
+        private Date debutFinEnchere;
+        private Retrait lieuRetrait;
+        private int prixVente;
 
     public ArticleVenduDto() {
     }
 
-    public ArticleVenduDto(int noArticle, String nomArticle, Date dateFinEnchere, int miseAPrix, String vendeur, int noCategorie) {
+    public ArticleVenduDto(int noArticle, String nomArticle, Date dateFinEnchere, int miseAPrix, String vendeur, int noCategorie, String description, Date debutFinEnchere, Retrait lieuRetrait, int prixVente) {
         this.noArticle = noArticle;
         this.nomArticle = nomArticle;
         this.dateFinEnchere = dateFinEnchere;
         this.miseAPrix = miseAPrix;
         this.vendeur = vendeur;
         this.noCategorie = noCategorie;
+        this.description = description;
+        this.debutFinEnchere = debutFinEnchere;
+        this.lieuRetrait = lieuRetrait;
+        this.prixVente = prixVente;
     }
 
     public int getNoArticle() {
@@ -75,27 +83,63 @@ public class ArticleVenduDto {
         this.noCategorie = noCategorie;
     }
 
-    @Override
-    public String toString() {
-        return "ArticleVendueDto{" +
-                "noArticle=" + noArticle +
-                ", nomArticle='" + nomArticle + '\'' +
-                ", dateFinEnchere=" + dateFinEnchere +
-                ", miseAPrix=" + miseAPrix +
-                ", vendeur=" + vendeur +
-                ", categorieArticle=" + noCategorie +
-                '}';
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDebutFinEnchere() {
+        return debutFinEnchere;
+    }
+
+    public void setDebutFinEnchere(Date debutFinEnchere) {
+        this.debutFinEnchere = debutFinEnchere;
+    }
+
+    public Retrait getLieuRetrait() {
+        return lieuRetrait;
+    }
+
+    public void setLieuRetrait(Retrait lieuRetrait) {
+        this.lieuRetrait = lieuRetrait;
+    }
+
+    public int getPrixVente() {
+        return prixVente;
+    }
+
+    public void setPrixVente(int prixVente) {
+        this.prixVente = prixVente;
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         ArticleVenduDto that = (ArticleVenduDto) o;
-        return noArticle == that.noArticle && miseAPrix == that.miseAPrix && noCategorie == that.noCategorie && Objects.equals(nomArticle, that.nomArticle) && Objects.equals(dateFinEnchere, that.dateFinEnchere) && Objects.equals(vendeur, that.vendeur);
+        return noArticle == that.noArticle && miseAPrix == that.miseAPrix && noCategorie == that.noCategorie && prixVente == that.prixVente && Objects.equals(nomArticle, that.nomArticle) && Objects.equals(dateFinEnchere, that.dateFinEnchere) && Objects.equals(vendeur, that.vendeur) && Objects.equals(description, that.description) && Objects.equals(debutFinEnchere, that.debutFinEnchere) && Objects.equals(lieuRetrait, that.lieuRetrait);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(noArticle, nomArticle, dateFinEnchere, miseAPrix, vendeur, noCategorie);
+        return Objects.hash(noArticle, nomArticle, dateFinEnchere, miseAPrix, vendeur, noCategorie, description, debutFinEnchere, lieuRetrait, prixVente);
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleVenduDto{" +
+                "noArticle=" + noArticle +
+                ", nomArticle='" + nomArticle + '\'' +
+                ", dateFinEnchere=" + dateFinEnchere +
+                ", miseAPrix=" + miseAPrix +
+                ", vendeur='" + vendeur + '\'' +
+                ", noCategorie=" + noCategorie +
+                ", description='" + description + '\'' +
+                ", debutFinEnchere=" + debutFinEnchere +
+                ", lieuRetrait=" + lieuRetrait +
+                ", prixVente=" + prixVente +
+                '}';
     }
 }
