81cbaab6bf4440f46ef635e0760dc4618c8d03cd
1316-dev <mathieu.pineau@hotmail.fr>
2025-12-19 16:01:54 +0100

    init view sql

--- src/main/resources/sql/view-afficherVentesEnCours.sql
+++ src/main/resources/sql/view-afficherVentesEnCours.sql
@@ -11,22 +11,11 @@ from ARTICLES_VENDUS
 DROP VIEW IF EXISTS gestionEncheresMesVentes
 GO
 
-CREATE VIEW gestionEncheresMesVentes
-AS
-select no_article,nom_article, prix_initial, date_debut_encheres, date_fin_encheres, utilisateurs.pseudo AS vendeur, no_categorie
-from ARTICLES_VENDUS
-         inner join UTILISATEURS on UTILISATEURS.no_utilisateur = ARTICLES_VENDUS.no_utilisateur
-GO
 
 
 
-DROP VIEW IF EXISTS gestionEncheresMesAchats
-GO
-CREATE VIEW gestionEncheresMesAchats
-AS
-select distinct ARTICLES_VENDUS.no_article,nom_article, prix_initial, date_debut_encheres, date_fin_encheres, utilisateurs.pseudo AS vendeur, no_categorie, ENCHERES.no_utilisateur As acheteur
-from ARTICLES_VENDUS
-         inner join ENCHERES ON ARTICLES_VENDUS.no_article = ENCHERES.no_article
-         LEFT join UTILISATEURS on ARTICLES_VENDUS.no_utilisateur = UTILISATEURS.no_utilisateur
-GO
+
+
+
+
 
---
+++ src/main/resources/sql/view-gestionEncheresMesVentes.sql
@@ -0,0 +1,6 @@
+CREATE VIEW gestionEncheresMesVentes
+AS
+select no_article,nom_article, prix_initial, date_debut_encheres, date_fin_encheres, utilisateurs.pseudo AS vendeur, no_categorie
+from ARTICLES_VENDUS
+         inner join UTILISATEURS on UTILISATEURS.no_utilisateur = ARTICLES_VENDUS.no_utilisateur
+GO
